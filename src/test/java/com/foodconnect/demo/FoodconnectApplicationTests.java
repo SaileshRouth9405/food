5b8ab815e146217eef5c2266f3973faf6d1d1f62
SaileshRouth <saileshrouth45@gmail.com>
2023-04-11 16:25:14 +0530

    first commit

---
+++ src/test/java/com/foodconnect/demo/FoodconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodconnect.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
